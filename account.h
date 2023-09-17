2d6116dcd6b0a7745013a76c5be0cdf310825d1f
Will (Viktor) <131462475+vm2260@users.noreply.github.com>
2023-09-17 16:50:54 +0000

    done

--- account.h
+++ account.h
@@ -23,27 +23,47 @@ private:
 public:
     // Default constructor.
     // Initialize 'balance' to 0.
-    Account();
+    Account(){
+        balance = 0;
+    }
 
     // Parameterized constructor.
     // Initialize 'balance' with the provided 'initialBalance'.
-    Account(double initialBalance);
+    Account(double initialBalance){
+        balance = initialBalance;
+    }
 
     // Getter function to return the current balance.
-    double getBalance();
+    double getBalance(){
+        return balance;
+    }
 
     // Setter function to set the balance to a new value.
-    void setBalance(double newBalance);
+    void setBalance(double newBalance){
+        balance = newBalance;
+    }
+
 
     // Function to deposit an amount into the account.
-    void deposit(double amount);
+    void deposit(double amount){
+        balance += amount;
+    }
 
     // Function to withdraw an amount from the account.
-    void withdraw(double amount);
+    void withdraw(double amount){
+        if (amount > balance){
+            std::cout << "Insufficient funds.\n";
+        }
+        else{
+            balance -= amount;
+        }
+    }
 
     // Destructor.
     // Display a message indicating the account is being destroyed.
-    ~Account();
+    ~Account(){
+        std::cout << "Account with balance: " << balance << " is being destroyed.\n";
+    }
 };
 
 #endif // ACCOUNT_H
